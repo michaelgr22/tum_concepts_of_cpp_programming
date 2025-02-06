b27bb4cdf9158542c355e968235e776c0b3a296b
michaelgr22 <michael.graml22@outlook.de>
2025-02-06 15:30:09 +0100

    a02 header-files

---
+++ a02/header-files/math_operations.hpp
@@ -0,0 +1,12 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+#define ZERO_DIVISION_RESULT 0
+#define SUBSTRACTION_DISABLED 0
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+#endif
---
+++ a02/header-files/print_utils.hpp
@@ -0,0 +1,13 @@
+#ifndef PRINT_UTILS_H
+#define PRINT_UTILS_H
+
+#include "math_operations.hpp"
+
+inline int get42() {
+    return 42;
+}
+extern int magic;
+
+void print_message(const char* message);
+
+#endif
