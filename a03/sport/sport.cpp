14d8ba969dbc800e370fbdc155c77d5fe8e3488b
michaelgr22 <michael.graml22@outlook.de>
2025-02-08 15:02:32 +0100

    exercise 3 finished

--- a03/sport/sport.cpp
+++ a03/sport/sport.cpp
@@ -1,5 +1,60 @@
 #include "sport.hpp"
+#include <print>
 
 namespace sport {
-void sort(const unsigned ** /*points_start*/, const unsigned ** /*points_end*/, unsigned /*size*/) {}
+
+int find_index_max(const unsigned **points_start, const unsigned **times_start, unsigned size) {
+    int *max_points_indices = new int[size];
+    for (unsigned i = 0; i < size; i++) { max_points_indices[i] = -1; }
+    int max_points_indices_index = 0;
+
+    unsigned current_max_points = *points_start[0];
+
+    for (unsigned i = 1; i < size; i++) {
+        if (*points_start[i] > current_max_points) { current_max_points = *points_start[i]; }
+    }
+    for (unsigned i = 0; i < size; i++) {
+        if (*points_start[i] == current_max_points) {
+            max_points_indices[max_points_indices_index] = i;
+            max_points_indices_index++;
+        }
+    }
+    max_points_indices_index--;
+    //std::println("max_points_indices_index {}", max_points_indices_index);
+
+    //for (int i = 0; i < max_points_indices_index + 1; i++) { std::println("max index {}", max_points_indices[i]); }
+    //std::println("max_points_indices_index {}", max_points_indices_index);
+    if (max_points_indices_index < 1) {
+        int max_point_index = max_points_indices[0];
+        delete[] max_points_indices;
+        return max_point_index;
+    }
+
+    unsigned current_lowest_time = *times_start[max_points_indices[0]];
+    int index_to_return = max_points_indices[0];
+    for (int i = 0; i < max_points_indices_index + 1; i++) {
+        if (*times_start[max_points_indices[i]] < current_lowest_time) {
+            current_lowest_time = *times_start[max_points_indices[i]];
+            index_to_return = max_points_indices[i];
+        }
+    }
+
+    delete[] max_points_indices;
+    return index_to_return;
+}
+
+void sort(const unsigned **points_start, const unsigned **times_start, unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        //for (unsigned i = 0; i < size; i++) { std::println("order {}", *points_start[i]); }
+        int index_max_points = find_index_max(&points_start[i], &times_start[i], size - i);
+        index_max_points += i;
+        const unsigned *tmp_points = points_start[i];
+        const unsigned *tmp_times = times_start[i];
+        points_start[i] = points_start[index_max_points];
+        times_start[i] = times_start[index_max_points];
+        points_start[index_max_points] = tmp_points;
+        times_start[index_max_points] = tmp_times;
+    }
+}
+
 } // namespace sport
--- a03/stockpiles/stockpiles.cpp
+++ a03/stockpiles/stockpiles.cpp
@@ -1,6 +1,60 @@
 #include "stockpiles.hpp"
 
 namespace stockpiles {
-void init(Stockpile * /*stock_start*/, unsigned /*stock_len*/) {}
-bool serve(Stockpile * /*stock_start*/, unsigned /*stock_len*/, unsigned /*amount*/, Stockpile & /*which*/) {}
+int find_largest_stockpile_index(Stockpile *stock_start, unsigned stock_len) {
+    int *max_stockpile_indices = new int[stock_len];
+    for (unsigned i = 0; i < stock_len; i++) { max_stockpile_indices[i] = -1; }
+    int max_stockpile_indices_index = 0;
+
+    unsigned current_max_stockpile = *(stock_start[0].bottles);
+
+    for (unsigned i = 1; i < stock_len; i++) {
+        if (*(stock_start[i].bottles) > current_max_stockpile) { current_max_stockpile = *(stock_start[i].bottles); }
+    }
+    for (unsigned i = 0; i < stock_len; i++) {
+        if (*(stock_start[i].bottles) == current_max_stockpile) {
+            max_stockpile_indices[max_stockpile_indices_index] = i;
+            max_stockpile_indices_index++;
+        }
+    }
+    max_stockpile_indices_index--;
+    //std::println("max_points_indices_index {}", max_points_indices_index);
+
+    //for (int i = 0; i < max_points_indices_index + 1; i++) { std::println("max index {}", max_points_indices[i]); }
+    //std::println("max_points_indices_index {}", max_points_indices_index);
+    if (max_stockpile_indices_index < 1) {
+        int max_stockpile_index = max_stockpile_indices[0];
+        delete[] max_stockpile_indices;
+        return max_stockpile_index;
+    }
+
+    unsigned current_lowest_id = stock_start[0].id;
+    int index_to_return = max_stockpile_indices[0];
+    for (int i = 0; i < max_stockpile_indices_index + 1; i++) {
+        if (stock_start[max_stockpile_indices[i]].id < current_lowest_id) {
+            current_lowest_id = stock_start[max_stockpile_indices[i]].id;
+            index_to_return = max_stockpile_indices[i];
+        }
+    }
+
+    delete[] max_stockpile_indices;
+    return index_to_return;
+}
+
+void init(Stockpile * /*stock_start*/, unsigned /*stock_len*/) {
+    /*Stockpile *stock_pile = new Stockpile[stock_len];
+    for (unsigned i = 0; i < stock_len; i++) { stock_start[i] = stock_pile[i]; }
+    stock_start = stock_pile;*/
+}
+bool serve(Stockpile *stock_start, unsigned stock_len, unsigned amount, Stockpile &which) {
+    if ((stock_len) == 0) { return false; }
+    int largest_stockpile_index = find_largest_stockpile_index(stock_start, stock_len);
+
+    if (*(stock_start[largest_stockpile_index].bottles) < amount) { return false; }
+
+    *(stock_start[largest_stockpile_index].bottles) -= amount;
+    which = stock_start[largest_stockpile_index];
+    return true;
+}
+
 } // namespace stockpiles
